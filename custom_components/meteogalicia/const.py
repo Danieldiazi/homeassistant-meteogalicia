788864d4784c196f7a43c5c7bf626b67c0101030
dani <danieldiazdelaiglesia@gmail.com>
2022-10-09 17:53:06 +0200

    Add forecast precipitation probability

--- custom_components/meteogalicia/const.py
+++ custom_components/meteogalicia/const.py
@@ -1,12 +1,12 @@
 """Constants for the MeteoGalicia integration."""
 
 DOMAIN = "meteogalicia"
-
 INTEGRATION_NAME = "MeteoGalicia"
 
 FORECAST_MAX_TEMPERATURE = "Forecast max temp. "
 FORECAST_MIN_TEMPERATURE = "Forecast min temp. "
+FORECAST_RAIN_PROBABILITY = "Forecast precipitation probability. "
 
 URL_FORECAST_CONCELLO = "https://servizos.meteogalicia.gal/mgrss/predicion/jsonPredConcellos.action?idConc={}"
-
 URL_OBS_CONCELLO = "https://servizos.meteogalicia.gal/mgrss/observacion/observacionConcellos.action?idConcello={}"
+CONF_ID_CONCELLO = "id_concello"
--- custom_components/meteogalicia/sensor.py
+++ custom_components/meteogalicia/sensor.py
@@ -1,35 +1,34 @@
+"""The Sensor module for MeteoGalicia integration."""
 import sys
 import logging
 import async_timeout
-from enum import Enum
 import voluptuous as vol
 from homeassistant.exceptions import PlatformNotReady
 from homeassistant.components.switch import PLATFORM_SCHEMA
 from homeassistant.const import __version__
 from homeassistant.helpers.aiohttp_client import async_create_clientsession
 from homeassistant.helpers.entity import Entity
-
 from . import const
-
+from homeassistant.util import dt
 import homeassistant.helpers.config_validation as cv
 
-__version__ = "0.1"
+# __version__ = "0.2"
 
-CONF_ID_CONCELLO = "id_concello"
+# HEADERS = {
+#    "accept": "application/ld+json",
+#    "user-agent": f"HomeAssistant/{__version__}",
+# }
 
 _LOGGER = logging.getLogger(__name__)
 
-# Obtain config from configuration.yaml
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({vol.Required(CONF_ID_CONCELLO): cv.string})
+# Obtaining config from configuration.yaml
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {vol.Required(const.CONF_ID_CONCELLO): cv.string}
+)
 
 # URL for Meteo Galicia webservice
 URL = const.URL_FORECAST_CONCELLO
 
-
-HEADERS = {
-    "accept": "application/ld+json",
-    "user-agent": f"HomeAssistant/{__version__}",
-}
 
 """Run async_setup_platform"""
 
@@ -38,7 +37,7 @@ async def async_setup_platform(
     hass, config, add_entities, discovery_info=None
 ):  # pylint: disable=missing-docstring, unused-argument
 
-    id_concello = config[CONF_ID_CONCELLO]
+    id_concello = config[const.CONF_ID_CONCELLO]
 
     # id_concello must to have 5 chars and be a number
     if len(id_concello) != 5 or (not id_concello.isnumeric()):
@@ -76,13 +75,38 @@ async def async_setup_platform(
         ],
         True,
     )
+    _LOGGER.info(
+        "Added tomorrow forecast sensor for '%s' with id '%s'", name, id_concello
+    )
+    add_entities(
+        [MeteoGaliciaForecastRainByDaySensor(name, id_concello, "Today", 0, session)],
+        True,
+    )
+    _LOGGER.info(
+        "Added today forecast rain probability sensor for '%s' with id '%s'",
+        name,
+        id_concello,
+    )
+    add_entities(
+        [
+            MeteoGaliciaForecastRainByDaySensor(
+                name, id_concello, "Tomorrow", 1, session
+            )
+        ],
+        True,
+    )
+    _LOGGER.info(
+        "Added tomorrow forecast rain probability sensor for '%s' with id '%s'",
+        name,
+        id_concello,
+    )
 
     add_entities(
         [MeteoGaliciaTemperatureSensor(name, id_concello, session)],
         True,
     )
     _LOGGER.info(
-        "Added tomorrow forecast sensor for '%s' with id '%s'", name, id_concello
+        "Added weather temperature sensor for '%s' with id '%s'", name, id_concello
     )
 
 
@@ -190,6 +214,125 @@ class MeteoGaliciaForecastTemperatureMaxByDaySensor(
     def icon(self):
         """Return icon."""
         return "mdi:thermometer"
+
+    @property
+    def extra_state_attributes(self):
+        """Return attributes."""
+        return self._attr
+
+
+class MeteoGaliciaForecastRainByDaySensor(Entity):  # pylint: disable=missing-docstring
+    def __init__(self, name, id, forecast_name, forecast_day, session):
+        self._name = name
+        self.id = id
+        self.forecast_name = forecast_name
+        self.forecast_day = forecast_day
+        self.session = session
+        self._state = 0
+        self.connected = True
+        self.exception = None
+        self._attr = {}
+
+    """Run async update."""
+
+    async def async_update(self):
+        """Run async update ."""
+        information = []
+        connected = False
+        try:
+            async with async_timeout.timeout(10):
+                response = await self.session.get(URL.format(self.id))
+                if response.status != 200:
+                    self._state = "unavailable"
+                    _LOGGER.warning(
+                        "[%s] Possible API  connection  problem. Currently unable to download data from MeteoGalicia - HTTP status code %s",
+                        self.id,
+                        response.status,
+                    )
+                else:
+                    data = await response.json()
+                    # _LOGGER.info("Test '%s' : '%s'",   self.id, data.get("predConcello")["listaPredDiaConcello"],     )
+                    if data.get("predConcello") is not None:
+                        item = data.get("predConcello")["listaPredDiaConcello"][
+                            self.forecast_day
+                        ]
+                        field = "manha"
+                        hour = int(dt.now().strftime("%H"))
+                        if hour > 21:
+                            field = "noite"
+                        elif hour > 14:
+                            field = "tarde"
+                        self._state = item.get("pchoiva", "null")[field]
+
+                        self._attr = {
+                            "information": information,
+                            "integration": "meteogalicia",
+                            "forecast_date": item.get("dataPredicion"),
+                            "rain_probability_noon": item.get("pchoiva", "null")[
+                                "manha"
+                            ],
+                            "rain_probability_afternoon": item.get("pchoiva", "null")[
+                                "tarde"
+                            ],
+                            "rain_probability_night": item.get("pchoiva", "null")[
+                                "noite"
+                            ],
+                            "id": self.id,
+                        }
+
+        except Exception:  # pylint: disable=broad-except
+            self.exception = sys.exc_info()[0].__name__
+            connected = False
+        else:
+            connected = True
+        finally:
+            # Handle connection messages here.
+            if self.connected:
+                if not connected:
+                    self._state = "unavailable"
+                    _LOGGER.warning(
+                        "[%s] Couldn't update sensor (%s)",
+                        self.id,
+                        self.exception,
+                    )
+
+            elif not self.connected:
+                if connected:
+                    _LOGGER.info("[%s] Update of sensor completed", self.id)
+                else:
+                    self._state = "unavailable"
+                    _LOGGER.warning(
+                        "[%s] Still no update available (%s)", self.id, self.exception
+                    )
+
+            self.connected = connected
+
+    @property
+    def name(self):
+        """Return the name."""
+        return f"{const.INTEGRATION_NAME} -  {self._name} - {self.forecast_name} - {const.FORECAST_RAIN_PROBABILITY}"
+
+    @property
+    def unique_id(self):
+        """Return a unique ID to use for this sensor."""
+        return f"{const.INTEGRATION_NAME.lower()}_{self._name}_{self.forecast_name.lower()}_{const.FORECAST_RAIN_PROBABILITY.lower()}_{self.id}".replace(
+            ",", ""
+        )
+
+    @property
+    def state(self):
+        """Return the state."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit_of_measurement."""
+        return "%"
+
+    @property
+    def icon(self):
+        """Return icon."""
+        return "mdi:percent"
 
     @property
     def extra_state_attributes(self):
