9f7c9155e50f461c3eec05ae1fa495b081f47763
pi <3638478+Danieldiazi@users.noreply.github.com>
2023-09-23 15:07:46 +0200

    Update const.py

--- custom_components/meteogalicia/const.py
+++ custom_components/meteogalicia/const.py
@@ -7,6 +7,7 @@ FORECAST_MAX_TEMPERATURE = "Forecast max temp. "
 FORECAST_MIN_TEMPERATURE = "Forecast min temp. "
 FORECAST_RAIN_PROBABILITY = "Forecast precipitation probability. "
 
+
 URL_FORECAST_CONCELLO = "https://servizos.meteogalicia.gal/mgrss/predicion/jsonPredConcellos.action?idConc={}"
 URL_OBS_CONCELLO = "https://servizos.meteogalicia.gal/mgrss/observacion/observacionConcellos.action?idConcello={}"
 CONF_ID_CONCELLO = "id_concello"
