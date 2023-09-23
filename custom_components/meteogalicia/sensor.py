29355f59594e8e3661bb2ed79acb167519ae93ab
pi <3638478+Danieldiazi@users.noreply.github.com>
2023-09-23 15:07:49 +0200

    Separadas las clases sensores de Estación para tener codigo más legible

--- custom_components/meteogalicia/sensor.py
+++ custom_components/meteogalicia/sensor.py
@@ -16,6 +16,7 @@ from homeassistant.components.sensor import (
     SensorStateClass,
 )
 
+from .StationSensorClass import MeteoGaliciaDailyDataByStationSensor, MeteoGaliciaLast10MinDataByStationSensor
 
 from meteogalicia_api.interface import MeteoGalicia
 
@@ -66,14 +67,14 @@ async def setup_id_estacion_platform(id_estacion, config, add_entities, session,
         
         if ((id_measure_daily is None and id_measure_last10min is None) or id_measure_daily is not None):
             add_entities(
-            [MeteoGaliciaDailyDataByStationSensor(id_estacion, id_estacion, id_measure_daily,session, hass)],
+            [MeteoGaliciaDailyDataByStationSensor(id_estacion, id_estacion, id_measure_daily,session, hass, ATTRIBUTION)],
             True,)
             _LOGGER.info(
             "Added daily data for '%s' with id '%s' - main measure is: %s", id_estacion, id_estacion, id_measure_daily)
         
         if ((id_measure_daily is None and id_measure_last10min is None) or id_measure_last10min is not None):
             add_entities(
-            [MeteoGaliciaLast10MinDataByStationSensor(id_estacion, id_estacion, id_measure_last10min,session, hass)],
+            [MeteoGaliciaLast10MinDataByStationSensor(id_estacion, id_estacion, id_measure_last10min,session, hass, ATTRIBUTION)],
             True,)
             _LOGGER.info(
             "Added last 10 min data for '%s' with id '%s' - main measure is: %s", id_estacion, id_estacion, id_measure_last10min)
@@ -195,7 +196,6 @@ def _get_observation_data_from_api(idc):
 
 async def get_forecast_data(hass, idc):
     """Poll weather data from MeteoGalicia API."""
-
     data = await hass.async_add_executor_job(_get_forecast_data_from_api, idc)
     return data
 
@@ -204,35 +204,6 @@ def _get_forecast_data_from_api(idc):
     """Call meteogalicia api in order to get obsertation data"""
     meteogalicia_api = MeteoGalicia()
     data = meteogalicia_api.get_forecast_data(idc)
-    return data
-
-
-
-async def get_observation_dailydata_by_station(hass, ids):
-    """Poll weather data from MeteoGalicia API."""
-
-    data = await hass.async_add_executor_job(_get_observation_dailydata_by_station_from_api, ids)
-    return data
-
-
-def _get_observation_dailydata_by_station_from_api(ids):
-    """Call meteogalicia api in order to get obsertation data"""
-    meteogalicia_api = MeteoGalicia()
-    data = meteogalicia_api.get_observation_dailydata_by_station(ids)
-    return data
-
-
-async def get_observation_last10mindata_by_station(hass, ids):
-    """Poll weather data from MeteoGalicia API."""
-
-    data = await hass.async_add_executor_job(_get_observation_last10mindata_by_station_from_api, ids)
-    return data
-
-
-def _get_observation_last10mindata_by_station_from_api(ids):
-    """Call meteogalicia api in order to get obsertation data"""
-    meteogalicia_api = MeteoGalicia()
-    data = meteogalicia_api.get_observation_last10mindata_by_station(ids)
     return data
 
 
@@ -547,240 +518,9 @@ class MeteoGaliciaTemperatureSensor(SensorEntity):  # pylint: disable=missing-do
         return TEMP_CELSIUS
 
 
-# Sensor Classget_observation_dailydata_by_station
-class MeteoGaliciaDailyDataByStationSensor(SensorEntity):  # pylint: disable=missing-docstring
-    """Sensor class."""
-
-    _attr_attribution = ATTRIBUTION
-    def __init__(self, name, ids, id_measure,session, hass):
-        self._name = name
-        self.id = ids
-        self.id_measure = id_measure
-        self.session = session
-        self._state = 0
-        self.connected = True
-        self.exception = None
-        self._attr = {}
-        self.hass = hass
-        if (id_measure is None):
-            self.name_suffix = ""
-        else:
-            self.name_suffix = "_"+id_measure
-        
-        #Set default value for measure_unit, because when a Timeout error appears sensor doesn't will create if "measure_unit" doesn't exists
-        self.measure_unit = None
 
 
-    async def async_update(self) -> None:
-        """Run async update ."""
-        information = []
-        connected = False
-        try:
-            async with async_timeout.timeout(const.TIMEOUT):
 
-                response = await get_observation_dailydata_by_station(self.hass, self.id)
-
-                if response is None or len(response.get("listDatosDiarios"))<=0:
-                    self._state = None
-                    _LOGGER.warning(
-                        "Currently unable to download asked data from MeteoGalicia: Or station id:%s doesn't exists or there are a possible API connection problem. ",
-                        self.id,
-                    )
-                    self.measure_unit = None
-                else:
-                   
-                    if response.get("listDatosDiarios") is not None:
-                        
-                        item = response.get("listDatosDiarios")[0]
-
-
-                        self._attr = {
-                            "information": information,
-                            "integration": "meteogalicia",
-                            "data": item.get("data"),
-                            #"utc_date": item.get("dataUTC"),
-                            "concello": item.get("listaEstacions")[0].get("concello"),
-                            "estacion": item.get("listaEstacions")[0].get("estacion"),
-                            "id": self.id,
-                            
-                        }
-                        self._name = item.get("listaEstacions")[0].get("estacion")
-                        lista_medidas = item.get("listaEstacions")[0].get("listaMedidas")
-                        
-                        self._attr = add_attributes_from_measures(lista_medidas, self._attr)
-                        self._state = get_state_station_sensor(self.id_measure, self._attr, self.id)
-                        self.measure_unit = get_measure_unit_station_sensor(self.id_measure, self._attr, self.id)
-                        
-                        
-        except Exception:  # pylint: disable=broad-except
-            self.exception = sys.exc_info()[0].__name__
-            _LOGGER.warning(
-                        const.STRING_NOT_UPDATE_SENSOR,
-                        self.id,
-                        self.exception,sys.exc_info()
-                    )
-            connected = False
-        else:
-            connected = True
-        finally:
-            self._state = check_connection(self.connected, connected, self._state, self.id, self.exception)
-            self.connected = connected
-
-    @property
-    def name(self) -> str:
-        """Return the name."""
-        return f"{self._name} - {self.name_suffix} - Station Daily Data" 
-
-    @property
-    def unique_id(self) -> str:
-        """Return a unique ID to use for this sensor."""
-        return f"meteogalicia_{self.id}_station_daily_data_{self.name_suffix.lower()}_{self.id}".replace(
-            ",", ""
-        )
-
-    @property
-    def icon(self):
-        """Return icon."""
-        return "mdi:information"
-
-    @property
-    def extra_state_attributes(self):
-        """Return attributes."""
-        return self._attr
-
-    @property
-    def native_value(self):
-        """Return the state of the sensor."""
-        return self._state
-
-        
-    @property
-    def native_unit_of_measurement(self) -> str:
-        """Return the unit_of_measurement."""
-        return self.measure_unit
-
-
-# Sensor Classget_observation_dailydata_by_station
-class MeteoGaliciaLast10MinDataByStationSensor(SensorEntity):  # pylint: disable=missing-docstring
-    """Sensor class."""
-
-    _attr_attribution = ATTRIBUTION
-    def __init__(self, name, ids, id_measure,session, hass):
-        self._name = name
-        self.id = ids
-        self.id_measure = id_measure
-        self.session = session
-        self._state = 0
-        self.connected = True
-        self.exception = None
-        self._attr = {}
-        self.hass = hass
-        if (id_measure is None):
-            self.name_suffix = ""
-        else:
-            self.name_suffix = "_"+id_measure
-        
-        #Set default value for measure_unit, because when a Timeout error appears sensor doesn't will create if "measure_unit" doesn't exists
-        self.measure_unit = None
-
-
-    async def async_update(self) -> None:
-        """Run async update ."""
-        information = []
-        connected = False
-        try:
-            async with async_timeout.timeout(const.TIMEOUT):
-
-                response = await get_observation_last10mindata_by_station(self.hass, self.id)
-                if response is None or len(response.get("listUltimos10min"))<=0:
-                    self._state = None
-                    _LOGGER.warning(
-                        "Currently unable to download asked data from MeteoGalicia: Or station id:%s doesn't exists or there are a possible API connection problem. ",
-                        self.id,
-                    )
-                    self.measure_unit = None
-                else:
-                   
-                    if response.get("listUltimos10min") is not None:
-                        
-                        item = response.get("listUltimos10min")[0]
-
-                        
-                        self._attr = {
-                            "information": information,
-                            "integration": "meteogalicia",
-                            "instanteLecturaUTC": item.get("instanteLecturaUTC"),
-                            "idEstacion": item.get("idEstacion"),
-                            "estacion": item.get("estacion"),
-                            "id": self.id,
-                            
-                        }
-                        
-                        self._name = item.get("estacion")
-                        lista_medidas = item.get("listaMedidas")
-                        self._attr = add_attributes_from_measures(lista_medidas, self._attr)
-                        self._state = get_state_station_sensor(self.id_measure, self._attr, self.id)
-                        self.measure_unit = get_measure_unit_station_sensor(self.id_measure, self._attr, self.id)
-                        
-
-        except Exception:  # pylint: disable=broad-except
-            self.exception = sys.exc_info()[0].__name__
-            _LOGGER.warning(
-                        const.STRING_NOT_UPDATE_SENSOR,
-                        self.id,
-                        self.exception,sys.exc_info()
-                    )
-            connected = False
-        else:
-            connected = True
-        finally:
-            self._state = check_connection(self.connected, connected, self._state, self.id, self.exception)
-            self.connected = connected
-
-    @property
-    def name(self) -> str:
-        """Return the name."""
-        return f"{self._name} - {self.name_suffix} - Station Last 10 min Data" 
-
-    @property
-    def unique_id(self) -> str:
-        """Return a unique ID to use for this sensor."""
-        return f"meteogalicia_{self.id}_station_last_10_min_data_{self.name_suffix.lower()}_{self.id}".replace(
-            ",", ""
-        )
-
-    @property
-    def icon(self):
-        """Return icon."""
-        return "mdi:information"
-
-    @property
-    def extra_state_attributes(self):
-        """Return attributes."""
-        return self._attr
-
-    @property
-    def native_value(self):
-        """Return the state of the sensor."""
-        return self._state        
-    @property
-    def native_unit_of_measurement(self) -> str:
-        """Return the unit_of_measurement."""
-        return self.measure_unit
-
-
-def add_attributes_from_measures(lista_medidas, attributes):
-    attr = attributes
-    for medida in lista_medidas:
-        #Chequeo si el dato recogido es válido o no.
-        #En la documentación 1 es dato valido original, y 5 dato valido interpolado
-        #Si el valor es -9999 es un valor inválido, por lo que no devolvemos el valor del atributo
-        if (medida.get("lnCodigoValidacion") in (1,5) ):
-            attr[medida.get("codigoParametro")+"_value"] = medida.get("valor")
-            attr[medida.get("codigoParametro")+"_unit"] = medida.get("unidade")
-        if (medida.get("valor") == -9999 ):
-            attr[medida.get("codigoParametro")+"_value"] = None
-    return attr
 
 def check_connection(self_connected, connected, self_state, self_id, self_exception):
     state = self_state
@@ -803,31 +543,6 @@ def check_connection(self_connected, connected, self_state, self_id, self_except
                 const.STRING_NOT_UPDATE_AVAILABLE, self_id, self_exception
             )
     return state
-
-def get_state_station_sensor(id_measure, attributes,id):
-    state = None
-    if (id_measure is None):
-        state = "Available"
-    else:
-        if id_measure+"_value" in attributes:
-            state = attributes[id_measure+"_value"]
-        else: #Measure for this sensor is unavailable
-            state = None
-            _LOGGER.warning("Couldn't update sensor with measure %s, it's unavailable for station id: %s", id_measure,id)
-    return state
-
-def get_measure_unit_station_sensor(id_measure, attributes,id):
-    measure_unit = None
-    if (id_measure is None):
-        measure_unit = None
-    else:
-        if id_measure+"_value" in attributes:
-            measure_unit = attributes[id_measure+"_unit"]
-        else: #Measure for this sensor is unavailable
-            measure_unit = None
-            _LOGGER.warning("Couldn't update sensor with measure %s, it's unavailable for station id: %s", id_measure,id)
-    return measure_unit
-
 
 def get_state_forecast_rain_by_day_sensor(max_value, item):
     state = None
