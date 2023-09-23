4c705d15fc04fddd3ccb4527bf4812ba40d79284
pi <3638478+Danieldiazi@users.noreply.github.com>
2023-09-23 15:07:45 +0200

    Create const.py

---
+++ custom_components/meteogalicia/const.py
@@ -0,0 +1,12 @@
+"""Constants for the Meteo Galicia integration."""
+
+DOMAIN = "meteogalicia"
+
+INTEGRATION_NAME = "Meteo Galicia"
+
+FORECAST_MAX_TEMPERATURE = "Forecast max temp. "
+FORECAST_MIN_TEMPERATURE = "Forecast min temp. "
+
+URL_FORECAST_CONCELLO = "https://servizos.meteogalicia.gal/mgrss/predicion/jsonPredConcellos.action?idConc={}"
+
+URL_OBS_CONCELLO = "https://servizos.meteogalicia.gal/mgrss/observacion/observacionConcellos.action?idConcello={}"
