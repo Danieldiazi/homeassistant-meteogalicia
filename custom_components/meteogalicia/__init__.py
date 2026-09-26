1ef53a1ef4ff418e2c762474671c2f026d4adae2
Daniel <3638478+Danieldiazi@users.noreply.github.com>
2026-09-26 19:07:01 +0200

    Add municipal weather warnings
    * Bump MeteoGalicia API to 0.1.4
    
    * Add weather warning binary sensor platform
    
    * Add warnings option constant
    
    * Add weather warning coordinators
    
    * Add weather warnings option to municipality entries
    
    * Add municipal warning level sensors
    
    * Add municipal weather warning binary sensor
    
    * Translate weather warning entities (en)
    
    * Translate weather warning entities (es)
    
    * Translate weather warning entities (gl)
    
    * Add weather warning tests

--- custom_components/meteogalicia/__init__.py
+++ custom_components/meteogalicia/__init__.py
@@ -8,7 +8,7 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 from .util import safe_close_coordinators
 
-PLATFORMS = ["sensor", "weather"]
+PLATFORMS = ["binary_sensor", "sensor", "weather"]
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
--- custom_components/meteogalicia/const.py
+++ custom_components/meteogalicia/const.py
@@ -23,6 +23,7 @@ CONF_ID_CONCELLO = "id_concello"
 CONF_ID_ESTACION = "id_estacion"
 CONF_ID_ESTACION_MEDIDA_DAILY = "id_estacion_medida_diarios"
 CONF_ID_ESTACION_MEDIDA_LAST10MIN = "id_estacion_medida_ultimos_10_min"
+CONF_WARNINGS_ENABLED = "warnings_enabled"
 
 # Timeout por defecto
 TIMEOUT = 60
